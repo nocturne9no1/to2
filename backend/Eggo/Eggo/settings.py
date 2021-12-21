2e3624ffacff9a1f3219c7c355cf41617fbf5a0e
Nocturne9no1 <nocturne9no1@gmail.com>
2021-12-21 10:57:23 +0900

    Fix: Hide secret key
    add secrets.json in .gitignore, change code setting.py

--- backend/Eggo/Eggo/settings.py
+++ backend/Eggo/Eggo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import json, os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&02$ynz2zul4t_kz_sllf#rmci&i%$lh=@8u1)(1pk+=0r4k6d'
+secret_file = os.path.join(BASE_DIR, 'SECRETS.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("secret_key")
+
+# SECRET_KEY = 'django-insecure-&02$ynz2zul4t_kz_sllf#rmci&i%$lh=@8u1)(1pk+=0r4k6d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
