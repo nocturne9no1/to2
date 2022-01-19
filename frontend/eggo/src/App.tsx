4e4250dc473479eafdc109faff8d3e8df9863e61
Nocturne9no1 <nocturne9no1@gmail.com>
2022-01-19 13:14:37 +0900

    Test: Slack Connect

--- frontend/eggo/src/App.tsx
+++ frontend/eggo/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Le arn test
+          For Slack connet Test
         </a>
       </header>
     </div>
