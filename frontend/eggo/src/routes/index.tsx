c0f7a97238835a3a77963d4eaf61b76a2eb92c89
Nocturne9no1 <nocturne9no1@gmail.com>
2022-01-21 00:11:17 +0900

    Add: React Router
    React Router 추가
    React Router Dom 추가
    각 항목 npm 사용하여 설치하였음
    @typescript 를 통하여 또한 설치하였음
    우선 Home, Test View 를 작성하여 필터 컴포넌트를 구성하며 테스트 해볼 것임

---
+++ frontend/eggo/src/routes/index.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { Redirect } from "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+
+// const Root: React.FC = () => (
+//   <BrowserRouter>
+//     <Switch>
+//       <Route path="/" exact component={Home} />
+//       <Route path="/test" component={Test} />
+//       <Redirect path="*" to="/" />
+//     </Switch>
+//   </BrowserRouter>
+// );
+
+// export default Root;
+
+export {};
