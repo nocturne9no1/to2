22e01c76b76940d4bc200ec9ff1c6f84ee41a0b3
Nocturne9no1 <nocturne9no1@gmail.com>
2022-01-22 23:48:42 +0900

    TO2-19 #on-progress

--- frontend/eggo/src/views/Test.tsx
+++ frontend/eggo/src/views/Test.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Test: React.FC = (): JSX.Element => {
-  return <h1>Test</h1>;
+  return (
+    <div className="grade-filter">
+      <div className="filter-body">
+        <div className="multi-range-slider">
+          <input
+            type="range"
+            name="input-left"
+            id="input-left"
+            min="0"
+            max="30"
+            value="0"
+          />
+          <input
+            type="range"
+            name="input-right"
+            id="input-right"
+            min="0"
+            max="30"
+            value="30"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Test;
