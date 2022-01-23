0149960c43f3cab965d0cea20ed233c19d165b1c
Nocturne9no1 <nocturne9no1@gmail.com>
2022-01-23 15:05:04 +0900

    Add: Slider Filter 스켈레톤 구현 완료
    TO2-19

--- frontend/eggo/src/views/Test.tsx
+++ frontend/eggo/src/views/Test.tsx
@@ -4,32 +4,24 @@ import "../scss/pages/_test.scss";
 const Test: React.FC = (): JSX.Element => {
   const [leftValue, setLeftValue] = useState("0");
   const [rightValue, setRightValue] = useState("30");
+  console.log(leftValue);
+  console.log(rightValue);
   const [range, setRange] = useState({
-    left: "25%",
-    right: "25%",
+    left: "0%",
+    right: "0%",
   });
   const [thumb, setThumb] = useState({
     left: "0%",
     right: "0%",
   });
 
-  // input range - unvisable
-  // const inputLeft = document.getElementById("input-left");
-  // const inputRight = document.getElementById("input-right");
-
-  // visable div
-  // const thumbLeft = document.querySelector(".sdlier > .thumb.left");
-  // const thumbRight = document.querySelector(".sdlier > .thumb.right");
-  // const range = document.querySelector(".slider > .range");
-
   const _setLeftValue = (e: React.FormEvent<HTMLInputElement>) => {
-    // setLeftValue(e.currentTarget.value);
     const value = parseInt(e.currentTarget.value);
     const [min, max] = [0, 30];
 
-    setLeftValue(String(Math.min(value, parseInt(rightValue) - 1)));
+    setLeftValue(String(Math.min(value, parseInt(rightValue))));
 
-    const percent = ((value - min) / (max - min)) * 100;
+    const percent = ((parseInt(leftValue) - min) / (max - min)) * 100;
     setThumb({ ...thumb, left: `${percent}%` });
     setRange({ ...range, left: `${percent}%` });
   };
@@ -38,11 +30,11 @@ const Test: React.FC = (): JSX.Element => {
     const value = parseInt(e.currentTarget.value);
     const [min, max] = [0, 30];
 
-    setRightValue(String(Math.max(value, parseInt(leftValue) + 1)));
+    setRightValue(String(Math.max(value, parseInt(leftValue))));
 
-    const percent = ((value - min) / (max - min)) * 100;
-    setThumb({ ...thumb, right: `${percent}%` });
-    setRange({ ...range, right: `${percent}%` });
+    const percent = ((parseInt(rightValue) - min) / (max - min)) * 100;
+    setThumb({ ...thumb, right: `${100 - percent}%` });
+    setRange({ ...range, right: `${100 - percent}%` });
   };
   return (
     <div className="grade-filter">
