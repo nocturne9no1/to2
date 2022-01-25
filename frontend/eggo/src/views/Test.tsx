8d25b5ca1a6ef17ddc03a9c6cfc6c1fa7ee77b0f
Nocturne9no1 <nocturne9no1@gmail.com>
2022-01-25 23:40:36 +0900

    Design: CSS 조금 수정
    TO2-19

--- frontend/eggo/src/views/Test.tsx
+++ frontend/eggo/src/views/Test.tsx
@@ -3,7 +3,7 @@ import "../scss/pages/_test.scss";
 
 const Test: React.FC = (): JSX.Element => {
   const [leftValue, setLeftValue] = useState("0");
-  const [rightValue, setRightValue] = useState("30");
+  const [rightValue, setRightValue] = useState("100");
   console.log(leftValue);
   console.log(rightValue);
   const [range, setRange] = useState({
@@ -17,7 +17,7 @@ const Test: React.FC = (): JSX.Element => {
 
   const _setLeftValue = (e: React.FormEvent<HTMLInputElement>) => {
     const value = parseInt(e.currentTarget.value);
-    const [min, max] = [0, 30];
+    const [min, max] = [0, 100];
 
     setLeftValue(String(Math.min(value, parseInt(rightValue))));
 
@@ -28,7 +28,7 @@ const Test: React.FC = (): JSX.Element => {
 
   const _setRightValue = (e: React.FormEvent<HTMLInputElement>) => {
     const value = parseInt(e.currentTarget.value);
-    const [min, max] = [0, 30];
+    const [min, max] = [0, 100];
 
     setRightValue(String(Math.max(value, parseInt(leftValue))));
 
@@ -45,7 +45,7 @@ const Test: React.FC = (): JSX.Element => {
             name="input-left"
             id="input-left"
             min="0"
-            max="30"
+            max="100"
             value={leftValue}
             onChange={(e) => _setLeftValue(e)}
           />
@@ -54,7 +54,7 @@ const Test: React.FC = (): JSX.Element => {
             name="input-right"
             id="input-right"
             min="0"
-            max="30"
+            max="100"
             value={rightValue}
             onChange={(e) => _setRightValue(e)}
           />
@@ -68,6 +68,30 @@ const Test: React.FC = (): JSX.Element => {
           ></div>
           <div className="thumb left" style={{ left: thumb.left }}></div>
           <div className="thumb right" style={{ right: thumb.right }}></div>
+        </div>
+        <div className="slider-label">
+          <div className="label bronze">
+            <p>Bronze</p>
+          </div>
+          <div className="label silver">
+            <div className="divider"></div>
+            <p>Silver</p>
+          </div>
+          <div className="label gold">
+            <div className="divider"></div>
+            <p>Gold</p>
+          </div>
+          <div className="label platinum">
+            <div className="divider"></div>
+            <p>Platinum</p>
+          </div>
+          <div className="label diamond">
+            <div className="divider"></div>
+            <p>Diamond</p>
+          </div>
+          <div className="label ruby">
+            <p>Ruby</p>
+          </div>
         </div>
       </div>
     </div>
